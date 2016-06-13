591d54f507d8861499d038d8414da7cb26514edb
hugomilan <hugofernando@gmail.com>
2016-06-13 00:36:20 -0400

    Update calling.m

--- Matlab/calling.m
+++ Matlab/calling.m
@@ -11,14 +11,12 @@ tic
 
 
 %% input variables
-%nameToOpen: Opens the selected stored file. See variables loadNow and readNow.
-% nameToOpen = 'plate_4BC_40e.tms.mat'; % 2D
+% nameToOpen: Opens the selected stored file. See variables loadNow and readNow.
+ nameToOpen = 'plate_4BC_40e.tms.mat'; % 2D
 % nameToOpen = 'plate_4BC_682e.tms.mat'; % 2D
 % nameToOpen = 'plate_4BC_10766e.tms.mat'; % 2D
-nameToOpen = 'plate_6BC_24e.tms.mat'; % 3D
 % nameToOpen = 'plate_6BC_206e.tms.mat'; % 3D
 % nameToOpen = 'plate_6BC_1368e.tms.mat'; % 3D
-% nameToOpen = 'plate_6BC_9618e.tms.mat'; % 3D
 % nameToOpen = 'plate_6BC_69941e.tms.mat'; % 3D
 
 % 1: will load into the workspace the data contained in the nameToOpen
